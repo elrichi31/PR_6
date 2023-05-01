22cc728ce88c765a16319cba73cf3d895ffffaad
elrichi31 <nicolawsmoina@gmail.com>
2023-05-01 01:08:52 -0500

    Add cache

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         String url = "https://nasasearch.nasa.gov/search?affiliate=nasa&page=6&query=%2A.jpg&sort_by=&utf8=%E2%9C%93";
         new DownloadTask(MainActivity.this).execute(url);
 
+        // Agrega este código para configurar el botón y el oyente de clic
+        Button openImageGridButton = findViewById(R.id.button_open_image_grid);
+        openImageGridButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImageGridActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     // Agrega este método a la clase MainActivity para configurar el adaptador
